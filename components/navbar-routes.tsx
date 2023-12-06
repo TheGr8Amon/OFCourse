e78c30679ad55d12b3800a79e92a428ca4bbcf52
TheGr8Amon <144041868+TheGr8Amon@users.noreply.github.com>
2023-12-06 14:46:12 +0800

    first contribution #polgwapo

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -28,17 +28,18 @@ export const NavbarRoutes = () => {
       <div className="flex gap-x-2 ml-auto">
         {isTeacherPage || isCoursePage ? (
           <Link href="/">
-            <Button size="sm" variant="ghost">
+            <Button size="sm" variant="chanix" >
               <LogOut className="h-4 w-4 mr-2" />
               Exit
             </Button>
           </Link>
         ) : isTeacher(userId) ? (
           <Link href="/teacher/courses">
-            <Button size="sm" variant="ghost">
-              Teacher mode
-            </Button>
-          </Link>
+  <Button size="sm" variant="chanix">
+    Teacher mode
+  </Button>
+</Link>
+
         ) : null}
         <UserButton
           afterSignOutUrl="/"
